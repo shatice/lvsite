543e8010147b1e6da827685a18b9d266a04561a1
shatice <sarah.gungor@hetic.net>
2019-11-28 22:58:07 +0100

    mobile test

--- src/js/script.js
+++ src/js/script.js
@@ -6,24 +6,24 @@ const movieTitles = document.querySelectorAll('.movieTitle');
 
 let el; 
 
-function toggleBg() {
-	document.querySelector('body').classList.toggle('is-changed-' + el.id);
-	document.querySelector('header').classList.toggle('is-changed');
-	document.querySelector('footer').classList.toggle('is-changed');
-	movieTitles.forEach(movieTitle => {
-		movieTitle.classList.toggle('is-changed');
-	});
-}
+// function toggleBg() {
+// 	document.querySelector('body').classList.toggle('is-changed-' + el.id);
+// 	document.querySelector('header').classList.toggle('is-changed');
+// 	document.querySelector('footer').classList.toggle('is-changed');
+// 	movieTitles.forEach(movieTitle => {
+// 		movieTitle.classList.toggle('is-changed');
+// 	});
+// }
 
-movieTitles.forEach(movieTitle => {
-	movieTitle.addEventListener('mouseover', function() {
-		el = movieTitle;
-		toggleBg();
-	})
-	movieTitle.addEventListener('mouseout', function() {
-		toggleBg();
-	})
-});
+// movieTitles.forEach(movieTitle => {
+// 	movieTitle.addEventListener('mouseover', function() {
+// 		el = movieTitle;
+// 		toggleBg();
+// 	})
+// 	movieTitle.addEventListener('mouseout', function() {
+// 		toggleBg();
+// 	})
+// });
 
 /*********** Menu **********/
 

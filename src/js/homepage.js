aec1b18c90e81b37f8ee392cbcf5c8659859a5ef
shatice <sarah.gungor@hetic.net>
2019-12-07 23:27:49 +0100

    improve js

---
+++ src/js/homepage.js
@@ -0,0 +1,53 @@
+/***** HOMEPAGE CONTENT *****/
+
+function hpMarkUp() {
+	const homeSection = document.getElementById('homeSection');
+
+	class homeMovie {
+		constructor(movieId, movieTitle) {
+			this.movieId = movieId; 
+			this.movieTitle = movieTitle;
+		}
+  }
+
+	const desiderare = new homeMovie ( 
+    'desiderare',
+		'DESIDERARE:<br>cor ne adito'
+	)
+
+	const apache = new homeMovie ( 
+    'apache',
+		`L'Apache`
+	)
+	
+	const raptus = new homeMovie ( 
+    'raptus',
+		`Raptus`
+	)
+	
+	const piscine = new homeMovie ( 
+    'piscine',
+		`5H MOINS 20<br>à la piscine`
+	)
+	
+	const chienne = new homeMovie ( 
+    'chienne',
+		`Chienne`
+	)
+	
+	const homeMovies = [desiderare, apache, raptus, piscine, chienne];
+  
+  homeMovies.forEach(homeMovie => {
+    const hpMarkUp = `
+			<article class="movie movie--${homeMovie.movieId}">
+			<div id="${homeMovie.movieId}" class="movieTitle">
+				<a href="./pages/${homeMovie.movieId}.html">${homeMovie.movieTitle}</a>
+			</div>
+			</article>
+    `;
+  
+    homeSection.innerHTML += hpMarkUp;
+  });
+}
+
+hpMarkUp(); 
--- src/js/script.js
+++ src/js/script.js
@@ -3,76 +3,33 @@ console.log('And its the right url');
 
 /*********** Home **********/
 
-const movieTitles = document.querySelectorAll('.movieTitle');
+function homeEffects() {
 
-let el; 
+	const movieTitles = document.querySelectorAll('.movieTitle');
 
-function toggleBg() {
-	document.querySelector('body').classList.toggle('is-changed-' + el.id);
-	document.querySelector('header').classList.toggle('is-changed');
-	document.querySelector('footer').classList.toggle('is-changed');
+	let el; 
+
+	function toggleBg() {
+		document.querySelector('body').classList.toggle('is-changed-' + el.id);
+		document.querySelector('header').classList.toggle('is-changed');
+		movieTitles.forEach(movieTitle => {
+			movieTitle.classList.toggle('is-changed');
+		});
+	}
+
 	movieTitles.forEach(movieTitle => {
-		movieTitle.classList.toggle('is-changed');
+		movieTitle.addEventListener('mouseover', function() {
+			console.log('OOOK'); 
+			el = movieTitle;
+			toggleBg();
+		})
+		movieTitle.addEventListener('mouseout', function() {
+			toggleBg();
+		})
 	});
+
 }
 
-movieTitles.forEach(movieTitle => {
-	movieTitle.addEventListener('mouseover', function() {
-		el = movieTitle;
-		toggleBg();
-	})
-	movieTitle.addEventListener('mouseout', function() {
-		toggleBg();
-	})
-});
+homeEffects(); 
 
-/*********** Menu **********/
-
-const nav = document.querySelector('.nav'),
-			menu = document.querySelector('.menu'), 
-			closeMenu = document.querySelector('.closeMenu'); 
-
-menu.addEventListener('click', function() {
-	menu.classList.toggle('is-clicked'); 
-	nav.classList.toggle('is-clicked');
-	movieTitles.forEach(movieTitle => {
-		movieTitle.classList.toggle('is-clicked');
-	});
-}); 
-
-// function openMovie() {
-// 	let movies = document.querySelectorAll('.movie');
-// 	let movieInfos = document.querySelector('.desiderareMovie');
-// 	let back = document.getElementById('back'); 
-// 	let moviesLinks = document.getElementById('moviesLinks'); 
-// 	let body = document.querySelector('body'); 
-
-// 	document.querySelector('.movieTitle p').addEventListener('click', function() {
-// 		console.log('ok'); 
-
-// 		back.classList.add('is-clicked'); 
-// 		movieInfos.classList.add('is-clicked'); 
-// 		moviesLinks.classList.add('is-removed'); 
-// 		body.classList.add('is-hidden');  
-	
-// 		// movies.forEach(movie => {
-// 		// 	movie.style.display = "none"; 
-// 		// });
-// 	})
-	
-// 	back.addEventListener('click', function() {
-// 		console.log('back'); 
-// 		back.classList.remove('is-clicked'); 
-// 		movieInfos.classList.remove('is-clicked'); 
-// 		moviesLinks.classList.remove('is-removed'); 
-// 		body.classList.remove('is-hidden'); 
-
-// 		// movies.forEach(movie => {
-// 		// 	movie.style.display = "flex"; 
-// 		// });
-	
-// 	})
-// }
-
-// openMovie(); 
 
